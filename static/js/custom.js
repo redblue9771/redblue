4c9e307253901e85872cfc98e144e567a9b72dad
redblue9771 <redblue9771@Outlook.com>
2019-03-01 01:12:13 +0800

    😘 新增评论和动态标题

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,36 +1,37 @@
-/**
- * 修复bootstrap轮播
- */
-(function (e) {
-  pangu.spacingPage();
-  'use strict';
-  $(function () {
-    // 获取手指在轮播图元素上的一个滑动方向（左右）
-    // 获取界面上轮播图容器
-    var $carousels = $('.carousel');
-    var startX, endX;
-    // 在滑动的一定范围内，才切换图片
-    var offset = 50;
-    // 注册滑动事件
-    $carousels.on('touchstart', function (e) {
-      // 手指触摸开始时记录一下手指所在的坐标x
-      startX = e.originalEvent.touches[0].clientX;
+pangu.spacingPage();
+window.dataLayer = window.dataLayer || [];
 
-    });
-    $carousels.on('touchmove', function (e) {
-      // 目的是：记录手指离开屏幕一瞬间的位置 ，用move事件重复赋值
-      endX = e.originalEvent.touches[0].clientX;
-    });
-    $carousels.on('touchend', function (e) {
-      //console.log(endX);
-      //结束触摸一瞬间记录手指最后所在坐标x的位置 endX
-      //比较endX与startX的大小，并获取每次运动的距离，当距离大于一定值时认为是有方向的变化
-      var distance = Math.abs(startX - endX);
-      if (distance > offset) {
-        //说明有方向的变化
-        //根据获得的方向 判断是上一张还是下一张出现
-        $(this).carousel(startX > endX ? 'next' : 'prev');
-      }
-    })
-  });
-}());
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'UA-123575709-1');
+
+// 动态标题
+
+var last_known_scroll_position = 0,
+    ticking = false,
+    currentTitle = document.getElementById("main-title").textContent;
+
+function doSomething(scroll_pos) {
+    let headerTitleHeight = document.getElementById("head").offsetHeight,
+        postTitle = document.getElementById("post-title").textContent,
+        navTitle = document.getElementById("main-title");
+    // do something with the scroll position
+    if (scroll_pos > headerTitleHeight) {
+        navTitle.innerText = postTitle;
+    } else {
+        navTitle.innerText = currentTitle;
+    }
+}
+
+window.addEventListener('scroll', function (e) {
+    last_known_scroll_position = window.scrollY;
+    if (!ticking) {
+        window.requestAnimationFrame(function () {
+            doSomething(last_known_scroll_position);
+            ticking = false;
+        });
+    }
+    ticking = true;
+});
